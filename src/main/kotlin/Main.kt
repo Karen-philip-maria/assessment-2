0532a47681591693832b1290811dbbfb527de218
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-03-25 08:25:09 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+    isPalindrome("bravo")
+    information("b")
+    numBers(12, 3,5)
+}
+// no.1
+
+fun information(name:String){
+    var sentence= "Barnie bakes brown bagels and buns"
+    println(sentence.removeSuffix(buildString{}))
+
+    // no.2
+    fun numBers(num1: Int, num2: Int, num3: Int): Array<Int> {
+        var a = num1
+        var b = num2
+        var c = num3
+        return arrayOf(a + b + c)
+        return arrayOf(numBers(num1, num2, num3.countOneBits())
+            return arrayOf(num1, num2, num3.)
+
+
+
+
+    }
+
+    // no.3
+
+    fun volumeSphere(num:Int){
+        var volume= 4/3 * 3.14159 *2*2*2
+        println(volume)
+
+    }    }
+
+
+// no.4
+
+fun isPalindrome(word:String):Boolean {
+    var word = "bravo"
+    if (word == "bravo"){
+        return true
+    } else {
+        return false
+    }
+}
